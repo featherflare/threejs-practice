a7aa3154db28e44a1aba873cae60792f47c66ae1
dachatorn <dachatorn.a@kegroup.co.th>
2022-10-03 11:48:26 +0700

    FirstPage

---
+++ src/pages/welcome/welcome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './welcome.css'
+
+export default function Welcome() {
+  return (
+    <div className='welcome'>
+      <p>
+        This <span>'Project'</span> create to test and practice 3D render in web
+      </p>
+    </div>
+  )
+}
